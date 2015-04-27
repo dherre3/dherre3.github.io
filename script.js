5e9fd91b509723cb6198f053001e47e7bbd013ab
David <davidfhr@hotmail.com>
2015-04-27 00:21:58 -0400

    Changes to favicon
    Adding JavaScript file!

---
+++ script.js
@@ -0,0 +1,17 @@
+function count_rabbits() {
+
+    for(var i=1; i<=3; i++) {
+
+        // operator + concatenates strings
+
+        alert("Rabbit "+i+" out of the hat!")
+
+    }
+
+}
+
+document.readyState(function(){
+	".aboutme".fadeOut("fast");
+	
+	
+});
