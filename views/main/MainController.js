a6268409ed80ddab62860c52d6aa8b9241ac4bdb
David Herrera <davidfherrerar@gmail.com>
2015-09-27 19:38:44 -0400

    Animation menu, color footer

--- views/main/MainController.js
+++ views/main/MainController.js
@@ -5,16 +5,18 @@ myWeb.controller('MainController', ['$anchorScroll', '$location', '$scope','$sta
 	$scope.menuOptions=['Home', 'Projects','Portafolio', 'Documents', 'Contacts'];
 	
 	$scope.showMenu=false;
-	$location.hash('anchor' + 1);
 	$scope.closeModal=function(){
+		$scope.animationMenu=='animated zoomOutUp';
 		$scope.rotateClass="animated fadeInDown";
 		$scope.showMenu=false;
 	}
 	$scope.openModal=function(){
 		$scope.rotateClass="animated fadeOutUp";
+		$scope.animationMenu=='animated bounce';
 		$scope.showMenu=true;
 	}
 	$scope.gotoSection = function(index) {
+		$scope.animationMenu=='animated zoomInDown';
 		$scope.showMenu=false;
 		$state.go($scope.menuOptions[index]);
       	$scope.rotateClass="animated fadeInDown";
